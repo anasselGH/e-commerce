f72c1bc8a33ab5620a4e70992d18bc96929b2ef2
m.barakat <meriembarakat30@gmail.com>
2024-05-12 02:09:44 +0200

    commandeee

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ModeReglementsController;
-// use App\Models\Famille;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FamilleController;
 use App\Http\Controllers\MarquesController;
